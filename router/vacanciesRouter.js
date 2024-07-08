d18720492423759922e3db2b89dfd50e08722e14
international911 <146134416+international911@users.noreply.github.com>
2024-07-08 04:15:48 +0300

    Delete router directory

--- router/vacanciesRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const vacanciesController = require('../controller/vacanciesController');
-
-router.get('/', (req, res) => {
-  vacanciesController.getVacancies(req, res);
-});
-
-module.exports = router;
